cec9850b2cd294916a7a0036e3dd12e3c7a3809d
devsks <coolnicesantosh@gmail.com>
2018-07-05 21:28:00 +0530

    new version

--- js/index.js
+++ js/index.js
@@ -84,3 +84,64 @@
     }
   });
 })(jQuery); // End of use strict
+var c = document.getElementById('c');
+var cxt = c.getContext("2d");
+
+c.width = window.innerWidth;
+c.height = window.innerHeight;
+
+
+
+var code = '#include <iostream>\
+using namespace std;\
+int main()\
+{ \
+   string s1,s2,s3,s4,s5;\
+   s1="\xF0\x9F\x98\x83";\
+   s2="\xF0\x9F\x92\x90";\
+   s3="\xF0\x9F\x8E\x89";\
+   s4="\xF0\x9F\x8E\x8A";\
+   s5="\xF0\x9F\x98\x98";\
+   cout<<"Happyyy Birthdayyy Santosh!!!"+s1+" "+s1+" "+s1<<endl;\
+    cout<<"I wish you get every happiness that you deserve "+s1+" "+s1+" "+s1<<endl;\
+    cout<<"Happy 23rd b\'day ";\
+    short i=23;\
+    while(i--)\
+    {\
+	cout<<s5<<" ";\
+    }\
+    cout<<endl;\
+    return 0;\
+}';
+chinese = code.split("");
+
+var font_size =10;
+var columns = c.width/font_size; 
+
+var drops = [];
+
+for(var x=0;x<columns;x++){
+  drops[x]=1;
+}
+
+function draw(){
+  cxt.fillStyle="rgba(0,0,0,0.05)";
+  cxt.fillRect(0,0,c.width,c.height);
+  
+  cxt.fillStyle = "#0F0";
+  cxt.font = font_size+'px arial';
+  
+  
+  for(var i=0;i<drops.length;i++){
+    var text = chinese[Math.floor(Math.random()*chinese.length)];
+    cxt.fillText(text,i*font_size,drops[i]*font_size);
+    
+    if(drops[i]*font_size>c.height && Math.random() >0.975)
+      drops[i]=0;
+    
+    //increment y coordinate
+    drops[i]++;
+}
+  
+}
+setInterval(draw,63);
