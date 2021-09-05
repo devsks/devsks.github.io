ab8e1be3baa62a7e7fa4cfeab3576e32c6056b00
Santosh Kumar Shaw <coolnicesantosh@gmail.com>
2021-09-05 16:10:56 +0000

    updated profile

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var languages = ["C","C++","JS","JAVA","Python","Android","HTML5","PHP"];
+var languages = ["C","C++","JS","C#","Python","Android","HTML5","PHP"];
 d3.selectAll(".service-box").data(languages).append("H3").text(function(d){
     return d;
 });
